f1ecd2f8dbb17cf5171c678074773686845d3943
Ole Friis Østergaard <olefriis@gmail.com>
2026-02-15 20:41:16 +0100

    Add Android Install button, disable text selection

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker for Stunt Car Racer PWA
 // Caches game files for offline play and home-screen launch
 
-var CACHE_NAME = 'scr-v15';
+var CACHE_NAME = 'scr-v17';
 var ASSETS = [
   'source.html',
   'source.js',
